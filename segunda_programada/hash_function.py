fe2dd9ac9ed6fabdc2588dd915335b31771a9391
Sebastian Casanova <sebastian.casanova@ulead.ac.cr>
2025-03-01 22:36:45 -0600

    Update hash_function.py

--- segunda_programada/hash_function.py
+++ segunda_programada/hash_function.py
@@ -1,8 +1,4 @@
 def compute_hash(fecha, cuarto, local):
-    """
-    Ejemplo: suma los ord() de la fecha (si existe) y el local,
-    y multiplica por (cuarto+1).
-    """
     total = 0
     for ch in str(fecha) + str(local):
         total += ord(ch)
